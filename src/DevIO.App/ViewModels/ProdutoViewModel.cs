e3665861515197f00e7f1da1a3a80c4b87eace6a
stenio-s <steniogm@gmail.com>
2021-05-06 15:38:17 -0300

    Criando as Controller e Views através do processo de Scaffolding

--- src/DevIO.App/ViewModels/ProdutoViewModel.cs
+++ src/DevIO.App/ViewModels/ProdutoViewModel.cs
@@ -13,6 +13,10 @@ namespace DevIO.App.ViewModels
         [Key]
         public Guid Id { get; set; }
 
+        [DisplayName("Fornecedor")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public Guid FornecedorId { get; set; }
+
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} carateres", MinimumLength = 3)]
         public string Nome { get; set; }
@@ -23,7 +27,8 @@ namespace DevIO.App.ViewModels
         public string Descricao { get; set; }
 
         public IFormFile ImagemUpload { get; set; }
-        //public string Imagem { get; set; }
+
+        public string Imagem { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public decimal Valor { get; set; }
@@ -35,5 +40,7 @@ namespace DevIO.App.ViewModels
         public bool Ativo { get; set; }
 
         public FornecedorViewModel Fornecedor { get; set; }
+
+        public IEnumerable<FornecedorViewModel> Fornecedores { get; set; }
     }
 }
